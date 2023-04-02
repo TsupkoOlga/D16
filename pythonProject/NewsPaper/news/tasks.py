52a4eb7a920f88b7264dbe46ec8f09b5a7a5d565
TsupkoOlga <tsupko2000@gmail.com>
2023-04-02 18:59:47 +0600

    Еженедельная рассылка

--- pythonProject/NewsPaper/news/tasks.py
+++ pythonProject/NewsPaper/news/tasks.py
@@ -1,7 +1,9 @@
+import datetime
 from celery import shared_task
 from django.template.loader import render_to_string
 from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
+from news.models import Post, Category
 
 @shared_task
 def send_notifications(preview, pk, title, subscribers):
@@ -15,6 +17,31 @@ def send_notifications(preview, pk, title, subscribers):
 
     msg = EmailMultiAlternatives(
         subject=title,
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers
+    )
+
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
+
+@shared_task
+def my_job():
+    today = datetime.datetime.now()
+    last_week = today - datetime.timedelta(days=7)
+    posts = Post.objects.filter(time_in__gte=last_week)
+    categories = set(posts.values_list('category__name', flat=True))
+    subscribers = set(Category.objects.filter(name__in=categories).values_list('subscribers__email', flat=True))
+    html_content = render_to_string(
+        'dayly_post.html',
+        {
+            'link': settings.SITE_URL,
+            'posts': posts,
+        }
+    )
+
+    msg = EmailMultiAlternatives(
+        subject='Статьи за неделю',
         body='',
         from_email=settings.DEFAULT_FROM_EMAIL,
         to=subscribers
